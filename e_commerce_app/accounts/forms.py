eab8cf80de192a9e4111b8947670e0250af5820d
Khushi Monga <khushimonga20@gmail.com>
2026-06-17 12:39:58 +0530

    add user registration functionality

---
+++ e_commerce_app/accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(
+                "An account with this email already exists."
+            )
+
+        return email
